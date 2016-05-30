eba04d5b00cc14c82c7bd2641b2e0093f2f5bc4a
= <leejh10003@gmail.com>
2016-05-31 07:04:01 +0900

    Searching db connection implemented

--- search_handler.php
+++ search_handler.php
@@ -1,3 +1,41 @@
 <?php
-    echo $_GET['title'] . $_GET['course_id'] . $_GET['credits'] . $_GET['dept_name'];
+    $title = $_GET['title'];
+    $course_id = $_GET['course_id'];
+    $credits = $_GET['credits'];
+    $dept_name = $_GET['dept_name'];
+    $dbconn = pg_connect("host=localhost dbname=joonji user=joonji password=Male8723%")
+        or die('db 시스템에 연결할 수 없습니다. 관리자에게 문의하십시오: ' . pg_last_error());
+        
+    $query = "select * from course where ";
+    $concat = "";
+    if(strlen($title . $course_id . $credits . $dept_name) === 0){
+        echo "<script>alert('잘못된 입력입니다.');history.back();</script>";
+    }
+    if($title != ""){
+        $concat = $concat . "and title like '%" . $title . "%' ";
+    }
+    if($course_id != ""){
+        $concat = $concat . "and course_id like '%" . $course_id . "%' ";
+    }
+    if($credits != ""){
+        $concat = $concat . "and credits=" . $credits . " ";
+    }
+    if($dept_name != ""){
+        $concat = $concat . "and dept_name like '%" . $dept_name . "%' ";
+    }
+    $query .= substr($concat, 3);
+    $query = substr($query, 0, strlen($query) - 1) . ';';
+    $result = pg_query($query)
+        or die('db에 문제가 발생했습니다. 관리자에게 문의하십시오.: ' . pg_last_error());
+
+    
+    echo "<table>\n";
+    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+        echo "\t<tr>\n";
+        foreach ($line as $col_value) {
+            echo "\t\t<td>$col_value</td>\n";
+        }
+        echo "\t</tr>\n";
+    }
+    echo "</table>\n";
 ?>
