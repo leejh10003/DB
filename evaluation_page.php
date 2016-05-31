4b195060e0ecfe583a9e9ea1e09a0c196e7c467f
= <leejh10003@gmail.com>
2016-06-01 02:47:40 +0900

    evaluation button implemented

--- evaluation_page.php
+++ evaluation_page.php
@@ -8,12 +8,12 @@
     $dbconn = pg_connect("host=localhost dbname=joonji user=joonji password=Male8723%")
         or die('db 시스템에 연결할 수 없습니다. 관리자에게 문의하십시오: ' . pg_last_error());
         
-    $query_base = "from evaluation natural join section natural join teach natural join prof natural join course where course_id='" . $course_id . "' and prof_id='" . $prof_id . "' and sec_id='" . $sec_id . "' and year=" . $year . " and semester=" . $semester . ";";
-    $query = "select * " . $query_base;
-    $query_avg_assignment = "select avg(assignment_amount) " . $query_base;
-    $query_avg_achievement = "select avg(achievement) " . $query_base;
-    $query_avg_learning = "select avg(learning) " . $query_base;
-    $query_avg_test_difficulty = "select avg(test_difficulty) " . $query_base;
+    $query_base = "from evaluation natural join section natural join teach natural join prof natural join course where course_id='" . $course_id . "' and prof_id='" . $prof_id . "' and sec_id='" . $sec_id . "' and year=" . $year . " and semester=" . $semester;
+    $query = "select * " . $query_base . ";";
+    $query_avg_assignment = "select avg(assignment_amount) " . $query_base . ";";
+    $query_avg_achievement = "select avg(achievement) " . $query_base . ";";
+    $query_avg_learning = "select avg(learning) " . $query_base . ";";
+    $query_avg_test_difficulty = "select avg(test_difficulty) " . $query_base . ";";
     $result = pg_query($query)
         or die('db에 문제가 발생했습니다. 관리자에게 문의하십시오.: ' . pg_last_error());
     $avg_assignment = number_format(pg_fetch_array(pg_query($query_avg_assignment), null, PGSQL_ASSOC)['avg'],2)
@@ -44,4 +44,13 @@
         echo "</tr>";
         }
     echo "</table>\n";
+    $query_written_eval = "select * from evaluation where member_id='" . $_SESSION['member_id'] . "';";
+    $member_existence = pg_fetch_array(pg_query($query_written_eval), null, PGSQL_ASSOC)['member_id']
+        or die('db에 문제가 발생했습니다. 관리자에게 문의하십시오.: ' . pg_last_error());
+     if($member_existence === $_SESSION['member_id']){
+         echo "<button href=''>평가...</button>";
+     }
+     else{
+         echo "이미 평가 작성을 진행하셨습니다.";
+     }
  ?>
