1600e52a9b1ee0f48215f4143a6f0efbe71b1190
= <leejh10003@gmail.com>
2016-06-02 14:21:17 +0900

    all floating menu link added

--- write.php
+++ write.php
@@ -790,17 +790,20 @@ fieldset {
     }
 ?>
     <div id="container-floating">
+      <a href='./search.php'>
       <div class="nd3 nds" data-toggle="tooltip" data-placement="left" data-original-title="Reminder">
         <svg style="margin-top:8px;margin-left:8px;width:24px;height:24px" viewBox="0 0 24 24">
           <path fill="#ffffff" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
         </svg>
       </div>
+      </a>
+      <a href="./courseAdd.php">
       <div class="nd1 nds" data-toggle="tooltip" data-placement="left" data-original-title="Edoardo@live.it"><img class="reminder">
         <svg style="margin-top:8px;margin-left:8px;width:24px;height:24px" viewBox="0 0 24 24">
           <path fill="#ffffff" d="M18,22H6A2,2 0 0,1 4,20V4C4,2.89 4.9,2 6,2H7V9L9.5,7.5L12,9V2H18A2,2 0 0,1 20,4V20A2,2 0 0,1 18,22M14,20H16V18H18V16H16V14H14V16H12V18H14V20Z" />
       </svg>
       </div>
-
+      </a>
       <div id="floating-button" data-toggle="tooltip" data-placement="left" data-original-title="Create" onclick="newmail()">
         <p class="plus">+</p>
       </div>
