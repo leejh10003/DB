37a0e3031d9543c44024956af08c0a33d0e0f726
= <leejh10003@gmail.com>
2016-06-02 13:53:13 +0900

    courseAdd materialized

--- courseAdd_handle.php
+++ courseAdd_handle.php
@@ -8,12 +8,12 @@
         $dbconn = pg_connect("host=localhost dbname=joonji user=joonji password=Male8723%")
             or die('db 시스템에 연결할 수 없습니다. 관리자에게 문의하십시오: ' . pg_last_error());
             
-        $query = "select * from course where title='" . $title . "' and course_id='" . $course_id . "';";
+        $query = "select * from course where course_id='" . $course_id . "';";
         $result = pg_query($query)
             or die('db에 문제가 발생했습니다. 관리자에게 문의하십시오.: ' . pg_last_error());
         $result_fetched = pg_fetch_assoc($result);
         if($result_fetched['course_id'] === $course_id){
-            echo "<script>alert('이미 존재하는 과목입니다.');history.back();</script>";
+            echo "<script>alert('" . $result_fetched['course_id'] . "이미 존재하는 과목입니다.');history.back();</script>";
         }
         else{
             $course_insert_query = "INSERT INTO course VALUES ('" .$course_id. "', '" . $title . "', '" . $dept_name . "', " . $credits . ");";
